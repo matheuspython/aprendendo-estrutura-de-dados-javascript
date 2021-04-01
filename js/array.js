b5cb1d5b14e75b395d9bd50e8f0d504636ab8dd5
matheus souza <ruivorobado@gmail.com>
2021-04-01 18:01:52 -0300

    adicionando uso de for com array

--- js/array.js
+++ js/array.js
@@ -14,13 +14,20 @@ const  avgTempMay = 25.5;
 //indices          0     1    2   3   4        
 const avgTemp = [31.9, 35.3, 42, 38, 25.5];
 
-avgTemp.forEach((value, i) => console.log(`temperatura do mês ${i} é  ${value}`))
+avgTemp.forEach((value, i) => console.log(`temperatura do mês ${i} é  ${value}`));
 
 //formas de criar um array
 
 //array feito com construtor
-const daysOfWeek = new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday')
+const daysOfWeek = new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday');
 
 
 //forma mais comum de iniciar um array
-const daysOfWeek2 = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+const daysOfWeek2 = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+//exemplo de uso de array com for
+const fibonacci = [0, 1, 1];
+
+for(let i = 3; i<20; i++) fibonacci[i] = fibonacci[i -1 ] + fibonacci[i - 2];
+
+
